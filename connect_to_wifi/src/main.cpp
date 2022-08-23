4c241785e60317fcf11941f13f70dba003461e7f
unknown <inamulrehman09@gmail.co^C git config --global user.email inamulrehman09@gmail.com>
2022-08-23 10:27:48 +0500

    esp32

---
+++ connect_to_wifi/src/main.cpp
@@ -0,0 +1,33 @@
+#include<WiFi.h>
+
+const char *ssid = "Inam ul Rehman";
+const char *password = "in123456";
+
+void setup(){
+  Serial.begin(9600);
+  
+  WiFi.begin(ssid,password);
+  Serial.println("Connecting to wifi..");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("<");
+    delay(1000);
+    Serial.println("<connecting>");
+  }
+
+  Serial.println("Connected to Wifi");
+  Serial.print("SSID: ");
+  Serial.print(WiFi.SSID());
+  Serial.print(", RSSI: ");
+  Serial.println(WiFi.RSSI());
+  // Serial.print("Local Esp32 IP address: ");
+  // Serial.println(WiFi.localIP());
+
+
+}
+
+void loop(){
+
+
+}
