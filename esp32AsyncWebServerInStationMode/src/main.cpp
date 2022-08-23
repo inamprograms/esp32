298204b4764344d946c97f8643f55ce54ce7c89d
unknown <inamulrehman09@gmail.co^C git config --global user.email inamulrehman09@gmail.com>
2022-08-23 16:50:16 +0500

    http_get request in station mode

--- esp32AsyncWebServerInStationMode/src/main.cpp
+++ esp32AsyncWebServerInStationMode/src/main.cpp
@@ -1,10 +1,34 @@
-#include <Arduino.h>
-#include <AsyncWebServer.h>
+#include<WiFi.h>
+#include<ESPAsyncWebServer.h>
+
+const char *ssid = "Inam ul Rehman";
+const char *password = "in123456";
+
+AsyncWebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  WiFi.begin(ssid,password);
+  Serial.println("Connecting to Network...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("<");
+    delay(100);
+    Serial.println("<connecting>");
+  }
+  Serial.println("Connected to wifi.");
+  Serial.print("Local Ip: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    
+    request->send_P(200, "text", "Welcome to our HOME page");
+  });
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
